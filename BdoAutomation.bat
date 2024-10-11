50aefad9b2e8e2a265702dac59a4e50832402eb8
y3t1t1 <borremans.andre@gmail.com>
2024-10-12 01:10:27 +0200

    improve scenario add screen impode Readme

---
+++ BdoAutomation.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Change directory to the location of poc.py (si nécessaire)
+#
+::cd C:\chemin\vers\le\script
+
+:: Lancer poc.py avec Python situé dans C:\Program Files\Python310
+"C:\Program Files\Python310\python.exe" poc.py
+
+pause
